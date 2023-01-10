c91266ff7016c99b21dec3b21dbc44fdd5e0fcef
sahil <sahil242927.github@gmail.com>
2023-01-10 06:19:28 +0530

    splice and slice

--- javascript/slice-function.js
+++ javascript/slice-function.js
@@ -1,9 +1,22 @@
+/* 
+    Slice(startIndex, endIndex) : creates a new array, doesn't change the original array
+    Note: end index doesn't include.
+
+    const arr = [1, 2, 3, 4, 5, 6];    
+    arr.slice(2, 3)); // [3];
+    Example: index 3 is the value 4 in the array since it is the end index inside slice method, it doesn't include
+    
+*/
+
 const arr = [1, 2, 3, 4, 5, 6];
 console.log(arr.slice(2, 3)); // [3];
 
-// returns every elms from index 2
+// returning every elms from index 2,
 console.log(arr.slice(2)); // [3,4,5,6];
 
-// sed, indicating an offset from the end of the sequence. slice(-2)
+// indicating an offset from the end of the sequence. slice(-2)
 // extracts the last two elements in the sequence.
 console.log(arr.slice(2, -2)); // [3,4]
+
+// doesn't change the original array
+console.log(arr);
--- javascript/splice-function.js
+++ javascript/splice-function.js
@@ -1,14 +1,23 @@
-// splice returns the deleted array
+/* 
+    splice => it gives you adding, removing, updating value features. it changes the original array
+    splice(startIndex, deleteCount, item1, item2) 
+*/
+
 const arr = [1, 2, 3, 4, 5];
 
-// // add 6,7,8 at the end
-// arr.splice(arr.length, 0, 6, 7, 8);
-// console.log(arr);
-
-// // add 6,7,8 in front
-// arr.splice(0, 0, 6, 7, 8);
-// console.log(arr);
-
-// add 6,7,8 in front
-arr.splice(2, 1, 6, 7, 8); // [1,2,6,7,8,4,5]
+// Adding 6,7,8 at the end
+arr.splice(arr.length, 0, 6, 7, 8);
 console.log(arr);
+
+// Removing 1 element at index 3
+//  [1, 2, 3, 4, 5];
+const removedItem = arr.splice(3, 1);
+console.log(removedItem); // 4
+console.log(arr); //  [1, 2, 3, 5];
+
+// Replacing element at index 3 and adding item 10
+//  [1, 2, 3, 4, 5];
+console.log(`====================`);
+const replacedItem = arr.splice(3, 1, 10);
+console.log(replacedItem); // 4
+console.log(arr); //  [1, 2, 3, 10, 5];
